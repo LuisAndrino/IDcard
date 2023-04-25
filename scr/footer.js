2241c140bc239bb9b204d8cf613d1bae74212a08
Luis Andrino <67714289+LuisAndrino@users.noreply.github.com>
2023-04-25 16:38:58 +0000

    Project finished

--- scr/footer.js
+++ scr/footer.js
@@ -3,10 +3,10 @@ import React from "react"
 export default function Footer (){
     return (
         <footer className="footer">
-            <img src="../icons/facebook.png" />
-            <img src="../icons/github.png" />
-            <img src="../icons/steam-logo.png" />
-            <img src="../icons/twitter-sign.png" />
+            <a href="https://www.facebook.com/luisandrino21/"><img src="../icons/facebook.png" /></a>
+            <a href="https://github.com/LuisAndrino"><img src="../icons/github.png" /></a>
+            <a href="https://steamcommunity.com/profiles/76561198886070459/"><img src="../icons/steam-logo.png" /></a>
+            <a href="https://twitter.com/000000000000L0A"><img src="../icons/twitter-sign.png" /></a>
         </footer>
     ) 
 }
--- scr/main.js
+++ scr/main.js
@@ -7,19 +7,21 @@ export default function main(){
                 <h1>Luis Andrino</h1>
                 <h4 className="role">Software developer</h4>
                 <h6><a href="mailto:luisandrino3@gmail.com">Luisandrino3@gmail.com</a></h6>
-                <button className="btn-link"><img className="linkedin" src="../icons/linkedin.png" /> Linkedin</button>                
+                <button className="btn-link" type="button" ><img className="linkedin" src="../icons/linkedin.png" /> <a href="https://www.linkedin.com/in/luisandrino/" >Linkedin</a></button>                
             
             
-                <h3>About me</h3>
-                <p>Hello there, my name is [Your Name] and I am a student of Computer Science. I have always been fascinated by technology and its ability to solve complex problems, which is what drew me towards pursuing this field of study. As a CS student, I am passionate about programming languages, algorithms, and software development.
-
-                I enjoy working on projects that challenge me to think outside the box and find innovative solutions. I am a team player and enjoy collaborating with others to achieve a common goal. In my free time, I like to explore new technologies and experiment with different programming languages to expand my skills.
+                <h3 className="h3">About me</h3>
+                <p>Hello there, my name is Luis and I am a student of Computer Science. I have always been fascinated by technology and its ability to solve complex problems, which is what drew me towards pursuing this field of study. As a CS student, I am passionate about programming languages, algorithms, and software development.</p>
+                <br></br>
+                <p>I enjoy working on projects that challenge me to think outside the box and find innovative solutions. I am a team player and enjoy collaborating with others to achieve a common goal. In my free time, I like to explore new technologies and experiment with different programming languages to expand my skills.
                 </p>
                 
-                <h3>Interest</h3>
-                <p>Ever since I was a child, I have been fascinated by technology and the role it plays in shaping the world around us. As I grew older, I began to appreciate how much of an impact CS has on our daily lives, from the devices we use to the software we rely on.
-
-                As a CS student, my interests lie in programming languages, algorithms, and software engineering. I enjoy learning about new programming paradigms and exploring different frameworks to build innovative solutions. I am particularly interested in the application of CS in areas such as artificial intelligence, cybersecurity, and data science.</p>
+                <h3 className="h3">Interest</h3>
+                <p>
+                    Ever since I was a child, I have been fascinated by technology and the role it plays in shaping the world around us. As I grew older, I began to appreciate how much of an impact CS has on our daily lives, from the devices we use to the software we rely on. </p>
+                    <br></br>
+                <p>    As a CS student, my interests lie in programming languages, algorithms, and software engineering. I enjoy learning about new programming paradigms and exploring different frameworks to build innovative solutions. I am particularly interested in the application of CS in areas such as artificial intelligence, cybersecurity, and data science.
+                </p>
             </section>
            
         </main>
