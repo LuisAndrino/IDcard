f3264fcc2a5f629240e6a51fb9aa5df79bb2c1b1
Luis Andrino <67714289+LuisAndrino@users.noreply.github.com>
2023-04-25 17:30:06 +0000

    dadsa

--- nav.js
+++ nav.js
@@ -3,6 +3,6 @@ import React from "react"
 export default function Nav(){
     return(
     <nav>
-        <img src="./photo.jpg" className="img-me" /> 
+        <img src="photo.jpg" className="img-me" /> 
     </nav>)
 }
